135cf3e20dde7bb4d9f6dbd53c3b994f514c168c
CalefOso <calef75@gmail.com>
2022-08-12 01:00:16 -0400

    Second Commit

--- src/home/Home.js
+++ src/home/Home.js
@@ -17,20 +17,19 @@ const BASE_URL= "https://api.openweathermap.org"
 const lang = "es"
 const units = "metric" // metric for Celcius, imperial for Fahrenheit and empty for Kelvin
 
-var mainIcon = "01d"
 
 const Home = () => {
-    
-    const [data, setData] = useState([]);
-    return (
+
+    const [nameCity, setNameCity] = useState([]);
+    const [countryCity, setCountryCity] = useState([]);
+    const [tempText, setTempText] = useState([]);
+    const [descText, setDescText] = useState([]);
+    const [minTempText, setMinTempText] = useState([]);
+    const [maxTempText, setMaxTempText] = useState([]);
+
+    return ( 
         <View style = {styles.container}>
            
-            <View>
-                {/*TODO: Imagen*/}
-            </View>
-            <View>
-                {/*TODO: Datos*/}
-            </View>
             <View>
                 <TextInput
                 placeholder = "Ingresa el nombre de la cuidad"
@@ -40,34 +39,50 @@ const Home = () => {
                 }} />
             </View>
             <View>
-                {data && (
-                    <Text>{`${data.name}`}</Text>
-                )}
+             <Text style={styles.cityCountyStyle}>{nameCity}, {countryCity}</Text>
            </View>
-           
-            <Image 
+           <Image 
             style={styles.mainImageStyle}
             source={require("../assets/images/despejado_dia.png")}/>
+
+            <View>
+                <Text style={styles.tempStyle}>{`${Math.round(tempText)} °C`}</Text>
+                <Text style={styles.descStyle}>{descText}No sale descripcion</Text>{/*AQUI DEBERIA MOSTRAR LA DESCRIPCION OBTENIDA DEL DATA */}
+                <Text style={styles.dateStyle}>{new Date().toLocaleString()}</Text>
+                <View style = {styles.lineStyle}/>
+            </View>
+            
+            <View style = {{flexDirection: 'row', justifyContent:'space-around'}}>
+            <Text style={styles.minMaxStyle}>{`Min: ${Math.round(minTempText)} °C`}</Text>
+            <Text style={styles.minMaxStyle}>{`Max: ${Math.round(maxTempText)} °C`}</Text>
+            </View>
             
         </View>
     );
 
+    async function requestWeatherData(city){
+        const data = await fetch(`${BASE_URL}/data/2.5/weather?q=${city}&lang=${lang}&units=${units}&appid=${API_KEY.key}`)
+        .then(res => res.json()).catch(function(error) {
+            console.log('There has been a problem with your fetch operation: ' + error.message);
+             // ADD THIS THROW error
+              throw error;
+            });
+            setNameCity(data['name']);
+            setCountryCity(data['sys']['country']);
+            setTempText(data['main'] ['temp']);
+            setDescText(data['weather']['icon']);//AQUI ME SALE ERROR DICE "undefined"
+            setMinTempText(data['main']['temp_min']);
+            setMaxTempText(data['main']['temp_max']);
+        //data debuger
+        console.log(data);
+        console.log("API KEY: "+API_KEY.key);
+        console.log(descText);
+        console.log(minTempText);
+        console.log(maxTempText);    
+    }
+
 };
-async function requestWeatherData(city){
-    const data = await fetch(`${BASE_URL}/data/2.5/weather?q=${city}&lang=${lang}&units=${units}&appid=${API_KEY.key}`)
-    .then(res => res.json());
 
-    //data
-    console.log(data);
-    console.log("API KEY: "+API_KEY.key);
-
-    // toast just for testing
-    if (Platform.OS === 'android') {
-        ToastAndroid.show(`${data['main']['temp']}`, ToastAndroid.SHORT)
-      } else {
-        AlertIOS.alert(city);
-      }
-}
 
 
 const styles = StyleSheet.create({
@@ -88,8 +103,44 @@ const styles = StyleSheet.create({
     },
 
     mainImageStyle: {
-        width: 200,
-        height: 200,
-        alignSelf: 'center'      },
+        width: 250,
+        height: 250,
+        alignSelf: 'center'      
+    },
+
+    cityCountyStyle: {
+        color: '#FFFFFF',
+        fontSize: 40,
+        alignSelf: 'center',
+        fontWeight: 'bold',
+    },
+    tempStyle: {
+        color: '#FFFFFF',
+        fontSize: 100,
+        alignSelf: 'center',
+        fontWeight: 'bold',
+
+    },
+    descStyle: {
+        color: '#FFF',
+        fontSize: 35,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
+    dateStyle: {
+        color: '#FFF',
+        marginTop: 10,
+        fontSize: 15,
+        alignSelf: 'center',
+    },
+    lineStyle: {
+        borderWidth: 0.5,
+        borderColor: '#FFF',
+        margin: 20,
+    },
+    minMaxStyle:{
+        color: '#FFF',
+        fontSize: 20,
+    },
 });
 export default Home; 
